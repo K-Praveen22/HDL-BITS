8090f20f7d3bfabe60b4c92377979ebb18e0cf75
K-Praveen22 <praveen22daya@gmail.com>
2024-09-27 13:22:20 +0530

    Update 3 VECTOR PART SELECT.v

--- 2 VERILOG LANGUAGE/2.2 VECTORS/3 VECTOR PART SELECT.v
+++ 2 VERILOG LANGUAGE/2.2 VECTORS/3 VECTOR PART SELECT.v
@@ -5,5 +5,5 @@ module top_module(
     assign out[31:24]=in[7:0];
     assign out[23:16]=in[15:8];
     assign out[15:8]=in[23:16];
-    assign out[7:0]=in[31:0];
+    assign out[7:0]=in[31:24];
     endmodule
