f06577110e08c9c60412657b8da01767b29cd422
K-Praveen22 <praveen22daya@gmail.com>
2024-10-02 17:49:24 +0530

    Update 4 THREE MODULES.v

--- MODULES; HIERARCHY/4 THREE MODULES.v
+++ MODULES; HIERARCHY/4 THREE MODULES.v
@@ -1,6 +1,6 @@
 module top_module ( input clk, input d, output q );
     wire a,b;
     my_dff dff0(clk,d,a);
-    my_dff dff_1(clk,a,b);
-    my_dff dff_2(clk,b,q);
+    my_dff dff1(clk,a,b);
+    my_dff dff2(clk,b,q);
 endmodule
